861303a607b5af2329d1fd6b8eac46b5ad2aec1f
Ferenc Győri <ferenc.gyori@lombiq.com>
2021-10-25 11:02:33 +0200

    Adding doc to HideRecipesByTagsFromSetup

--- Lombiq.Hosting.Tenants.Management/Extensions/LombiqHostingTenantsManagementExtensions.cs
+++ Lombiq.Hosting.Tenants.Management/Extensions/LombiqHostingTenantsManagementExtensions.cs
@@ -6,6 +6,11 @@ namespace Lombiq.Hosting.Tenants.Management.Extensions
 {
     public static class LombiqHostingTenantsManagementExtensions
     {
+        /// <summary>
+        /// Registers Lombiq.Hosting.Tenants.Management.HideRecipesFromSetup as a setup feature and configure a set of
+        /// recipe tags which will be hidden on the setup screen.
+        /// </summary>
+        /// <param name="tags">Recipe tags which will be hidden.</param>
         public static OrchardCoreBuilder HideRecipesByTagsFromSetup(this OrchardCoreBuilder builder, params string[] tags) =>
             builder
                 .AddSetupFeatures(HideRecipesFromSetup)
