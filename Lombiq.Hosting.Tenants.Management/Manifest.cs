9f94a27013c4b55420f750e915fe2c73dec32b10
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-17 19:49:31 +0100

    Updating version number

--- Lombiq.Hosting.Tenants.Management/Manifest.cs
+++ Lombiq.Hosting.Tenants.Management/Manifest.cs
@@ -5,7 +5,7 @@ using static Lombiq.Hosting.Tenants.Management.Constants.FeatureNames;
     Name = "Lombiq Hosting - Tenants Management",
     Author = "Lombiq Technologies",
     Website = "https://github.com/Lombiq/Hosting-Tenants",
-    Version = "1.0.0",
+    Version = "2.0.0",
     Description = "Manage restrictions on tenant creation."
 )]
 
