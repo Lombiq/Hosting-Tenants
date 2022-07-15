080b0f78865eb7c84f8134f4ea166e64c8368e7e
Tamas Vincze <tamas.vincze@lombiq.com>
2022-07-15 11:44:37 +0200

    Adding MultiTenancy.Tenants module

---
+++ Lombiq.Hosting.MultiTenancy.Tenants/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Lombiq.Hosting.MultiTenancy.Tenants",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Lombiq.Hosting.MultiTenancy.Tenants",
+    Category = "Content Management"
+)]
