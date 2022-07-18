ac775badad0a6e297d7bc62b288ed7d66751e19d
Tamas Vincze <tamas.vincze@lombiq.com>
2022-07-18 16:29:52 +0200

    Correcting info in the Manifest

--- Lombiq.Hosting.MultiTenancy.Tenants/Manifest.cs
+++ Lombiq.Hosting.MultiTenancy.Tenants/Manifest.cs
@@ -1,10 +1,10 @@
 using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
-    Name = "Lombiq.Hosting.MultiTenancy.Tenants",
+    Name = "Lombiq Hosting MultiTenancy Tenants",
     Author = "The Orchard Core Team",
-    Website = "https://orchardcore.net",
+    Website = "https://github.com/Lombiq/Hosting-Tenants",
     Version = "0.0.1",
     Description = "Lombiq.Hosting.MultiTenancy.Tenants",
-    Category = "Content Management"
+    Category = "Hosting"
 )]
