4b6b0443d8c24528b19edee8b710e224abac8c0d
Tamas Vincze <tamas.vincze@lombiq.com>
2022-07-27 19:55:57 +0200

    Adding FeatureNames

---
+++ Lombiq.Hosting.MultiTenancy.Tenants/Constants/FeatureNames.cs
@@ -0,0 +1,8 @@
+namespace Lombiq.Hosting.MultiTenancy.Tenants.Constants;
+
+public class FeatureNames
+{
+    public const string Module = "Lombiq.Hosting.MultiTenancy.";
+
+    public const string Tenants = Module + "." + nameof(Tenants);
+}
