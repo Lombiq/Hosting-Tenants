adb5304b1b775fb312bcf170fdfc3bf393d6f0c2
Tamas Vincze <tamas.vincze@lombiq.com>
2022-08-12 17:13:08 +0200

    Removing unused Options

--- Lombiq.Hosting.MultiTenancy.Tenants/Settings/ForbiddenFeaturesOptions.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Lombiq.Hosting.MultiTenancy.Tenants.Settings;
-
-public class ForbiddenFeaturesOptions
-{
-    public IEnumerable<string> ForbiddenFeatureNames { get; set; }
-}
