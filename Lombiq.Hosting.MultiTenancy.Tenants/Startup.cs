703fc1a0991514fbfe0668b932c57187a84d3274
Tamas Vincze <tamas.vincze@lombiq.com>
2022-08-12 17:02:54 +0200

    Adding options, and service to Startup

--- Lombiq.Hosting.MultiTenancy.Tenants/Startup.cs
+++ Lombiq.Hosting.MultiTenancy.Tenants/Startup.cs
@@ -1,26 +1,38 @@
 using System;
 using Fluid;
+using Lombiq.Hosting.MultiTenancy.Tenants.Models;
+using Lombiq.Hosting.MultiTenancy.Tenants.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
 
-namespace Lombiq.Hosting.MultiTenancy.Tenants
-{
-    public class Startup : StartupBase
-    {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-        }
+namespace Lombiq.Hosting.MultiTenancy.Tenants;
 
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            routes.MapAreaControllerRoute(
-                name: "Home",
-                areaName: "Lombiq.Hosting.MultiTenancy.Tenants",
-                pattern: "Home/Index",
-                defaults: new { controller = "Home", action = "Index" }
-            );
-        }
+[Feature("FeaturesGuard")]
+public class Startup : StartupBase
+{
+    private readonly IShellConfiguration _shellConfiguration;
+
+    public Startup(IShellConfiguration shellConfiguration) => _shellConfiguration = shellConfiguration;
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.Configure<ForbiddenFeaturesOptions>(options =>
+            _shellConfiguration
+                .GetSection("Lombiq_Hosting_MultiTenancy_Tenants:ForbiddenFeaturesOptions")
+                .Bind(options));
+        services.Configure<AlwaysOnFeaturesOptions>(options =>
+            _shellConfiguration
+                .GetSection("Lombiq_Hosting_MultiTenancy_Tenants:AlwaysOnFeaturesOptions")
+                .Bind(options));
+        services.AddScoped<IFeaturesGuardService, FeaturesGuardService>();
+    }
+
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+       //builder.UseMiddleware<FeaturesGuardService>();
     }
 }
