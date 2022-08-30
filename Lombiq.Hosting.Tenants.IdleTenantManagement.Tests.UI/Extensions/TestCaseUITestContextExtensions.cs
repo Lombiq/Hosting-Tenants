87be246fd7bcfe09af3befda9817e53646b4bacd
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-08-30 20:25:59 +0200

    Adding UI test extension project

---
+++ Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -0,0 +1,20 @@
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using System.Threading.Tasks;
+
+namespace Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI.Extensions;
+
+public static class TestCaseUITestContextExtensions
+{
+    public static async Task TestIdleTenantManagerBehaviorAsync(this UITestContext context)
+    {
+        // We are letting the site to sit idle for more than a minute so that the
+        // tenant could be shut down by the background task.
+        await Task.Delay(69420);
+
+        // If we can access the admin menu after the tenant shut down that means the new shell was created
+        // and it is working as intended.
+        await context.SignInDirectlyAsync();
+        await context.GoToDashboardAsync();
+    }
+}
