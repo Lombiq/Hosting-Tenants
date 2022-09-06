dc601eaa6e2a87dd29916dca6abde11fb36cbda1
Gábor Pór <gabor.por@lombiq.com>
2022-09-06 11:27:06 +0200

    Removing FeaturesGuardService

--- Lombiq.Hosting.MultiTenancy.Tenants/Startup.cs
+++ Lombiq.Hosting.MultiTenancy.Tenants/Startup.cs
@@ -1,14 +1,10 @@
 using Lombiq.Hosting.MultiTenancy.Tenants.Handlers;
 using Lombiq.Hosting.MultiTenancy.Tenants.Models;
-using Lombiq.Hosting.MultiTenancy.Tenants.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Environment.Shell;
 using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
-using System;
 
 namespace Lombiq.Hosting.MultiTenancy.Tenants;
 
@@ -26,12 +22,6 @@ public class Startup : StartupBase
                 .GetSection("Lombiq_Hosting_MultiTenancy_Tenants:AlwaysEnabledFeaturesOptions")
                 .Bind(options));
 
-        services.AddScoped<IFeaturesGuardService, FeaturesGuardService>();
         services.AddScoped<IFeatureEventHandler, FeaturesEventHandler>();
-    }
-
-    public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-    {
-       //app.UseMiddleware<FeaturesGuardService>();
     }
 }
