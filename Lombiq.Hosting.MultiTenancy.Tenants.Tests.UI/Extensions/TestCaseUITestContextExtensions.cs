9ab2a20d234d99e55849aa78f5889ef110b77d7a
Gábor Pór <gabor.por@lombiq.com>
2022-09-27 20:36:08 +0200

    Creating tenant manually instead

--- Lombiq.Hosting.MultiTenancy.Tenants.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.MultiTenancy.Tenants.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,10 +1,8 @@
 using Lombiq.Hosting.MultiTenancy.Tenants.Tests.UI.Constants;
 using Lombiq.Tests.UI.Constants;
 using Lombiq.Tests.UI.Extensions;
-using Lombiq.Tests.UI.Models;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.MultiTenancy.Tenants.Tests.UI.Extensions;
@@ -15,49 +13,50 @@ public static class TestCaseUITestContextExtensions
     {
         await context.SignInDirectlyAndGoToDashboardAsync();
 
-        // Create and set up new tenant.
-        var tenantModel = new CreateTenant
-        {
-            FeatureProfile = "features guard",
-        };
+        // Create new tenant manually.
+        await context.CreateNewTenantManuallyAsync("Testenant", "tt1", "localhost", "features guard");
 
-        await context.CreateAndEnterTenantAsync("Testenant", "tt1", Recipes.OsoceTestsRecipeId, tenantModel);
+        // Setup newly created tenant.
+        await context.ClickReliablyOnAsync(By.LinkText("Setup"));
+        await context.ClickAndFillInWithRetriesAsync(By.Id("SiteName"), "Testenant");
+        context.ExecuteScript(
+            "document.querySelectorAll(\"a[data-recipe-name='" + Recipes.OsoceTestsRecipeId + "']\")[0].click()");
+        await context.ClickAndFillInWithRetriesAsync(By.Id("UserName"), DefaultUser.UserName);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("Email"), DefaultUser.Email);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("Password"), DefaultUser.Password);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("PasswordConfirmation"), DefaultUser.Password);
+        await context.ClickReliablyOnAsync(By.XPath("//button[@id='SubmitButton']"));
 
         // Log into tenant site and navigate to features list.
-        // Temporarily only go to admin and see if exception persists.
-        await context.GoToRelativeUrlAsync("/tt1");
+        await context.GoToRelativeUrlAsync("/tt1/admin/features");
+        await context.ClickAndFillInWithRetriesAsync(By.Id("UserName"), DefaultUser.UserName);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("Password"), DefaultUser.Password);
+        await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Log in')]"));
 
-#pragma warning disable AsyncFixer02 // Long-running or blocking operations inside an async method
-        Thread.Sleep(10000);
-#pragma warning restore AsyncFixer02 // Long-running or blocking operations inside an async method
-
-        await context.GoToRelativeUrlAsync("/tt1/admin");
-
-#pragma warning disable S125 // Sections of code should not be commented out
-        // await context.GoToRelativeUrlAsync("/tt1/admin/features");
-        // await context.ClickAndFillInWithRetriesAsync(By.Id("UserName"), DefaultUser.UserName);
-        // await context.ClickAndFillInWithRetriesAsync(By.Id("Password"), DefaultUser.Password);
-        // await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Log in')]"));
-
-        //// Ensure forbidden features are not available in the list.
-        // context.Missing(By.XPath("//label[@for='OrchardCore.Workflows.Session']"));
-        // context.Missing(By.XPath("//label[@for='OrchardCore.Lucene']"));
-        // context.Missing(By.XPath("//label[@for='OrchardCore.MiniProfiler']"));
-        // context.Missing(By.XPath("//label[@for='Lombiq.Tests.UI.Shortcuts']"));
-#pragma warning restore S125 // Sections of code should not be commented out
+        // Ensure forbidden features are not available in the list.
+        context.Missing(By.XPath("//label[@for='OrchardCore.Workflows.Session']"));
+        context.Missing(By.XPath("//label[@for='OrchardCore.Lucene']"));
+        context.Missing(By.XPath("//label[@for='OrchardCore.MiniProfiler']"));
+        context.Missing(By.XPath("//label[@for='Lombiq.Tests.UI.Shortcuts']"));
     }
 
     public static async Task TestAlwaysEnabledFeaturesAsync(this UITestContext context)
     {
         await context.SignInDirectlyAndGoToDashboardAsync();
 
-        // Create and set up new tenant.
-        var tenantModel = new CreateTenant
-        {
-            FeatureProfile = "features guard",
-        };
+        // Create new tenant manually.
+        await context.CreateNewTenantManuallyAsync("Testenant", "tt1", "localhost", "features guard");
 
-        await context.CreateAndEnterTenantAsync("Testenant", "tt1", Recipes.OsoceTestsRecipeId, tenantModel);
+        // Setup newly created tenant.
+        await context.ClickReliablyOnAsync(By.LinkText("Setup"));
+        await context.ClickAndFillInWithRetriesAsync(By.Id("SiteName"), "Testenant");
+        context.ExecuteScript(
+            "document.querySelectorAll(\"a[data-recipe-name='" + Recipes.OsoceTestsRecipeId + "']\")[0].click()");
+        await context.ClickAndFillInWithRetriesAsync(By.Id("UserName"), DefaultUser.UserName);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("Email"), DefaultUser.Email);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("Password"), DefaultUser.Password);
+        await context.ClickAndFillInWithRetriesAsync(By.Id("PasswordConfirmation"), DefaultUser.Password);
+        await context.ClickReliablyOnAsync(By.XPath("//button[@id='SubmitButton']"));
 
         // Log into tenant site and navigate to features list.
         await context.GoToRelativeUrlAsync("/tt1/admin/features");
