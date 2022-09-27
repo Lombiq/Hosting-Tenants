10884f7a831b37e76dd3daa1dc40e7a12c47c707
Gábor Pór <gabor.por@lombiq.com>
2022-09-27 11:27:24 +0200

    Fixing feature name

--- Lombiq.Hosting.MultiTenancy.Tenants/Constants/FeatureNames.cs
+++ Lombiq.Hosting.MultiTenancy.Tenants/Constants/FeatureNames.cs
@@ -2,7 +2,7 @@ namespace Lombiq.Hosting.MultiTenancy.Tenants.Constants;
 
 public static class FeatureNames
 {
-    public const string Module = "Lombiq.Hosting.MultiTenancy.";
+    public const string Module = "Lombiq.Hosting.MultiTenancy";
 
     public const string Tenants = Module + "." + nameof(Tenants);
 
