d026b08387a646963601257c759ee2871723b1d1
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-21 18:45:52 +0200

    Adapting to method rename

--- Lombiq.Hosting.Tenants.FeaturesGuard.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.Tenants.FeaturesGuard.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -40,7 +40,7 @@ public static class TestCaseUITestContextExtensions
 
         const string tenantName = "TestTenant";
 
-        await context.CreateAndChangeToTenantManuallyAsync(tenantName, "tt1", string.Empty, "features guard");
+        await context.CreateAndSwitchToTenantManuallyAsync(tenantName, "tt1", string.Empty, "features guard");
 
         await context.GoToSetupPageAndSetupOrchardCoreAsync(
             new OrchardCoreSetupParameters(context)
