2e57341acdd5f2dea3051ecbd2bfa60b2e670731
Gábor Pór <gabor.por@lombiq.com>
2022-10-27 13:25:54 +0200

    Cleaning up

--- Lombiq.Hosting.Tenants.FeaturesGuard/Handlers/FeaturesEventHandler.cs
+++ Lombiq.Hosting.Tenants.FeaturesGuard/Handlers/FeaturesEventHandler.cs
@@ -1,15 +1,13 @@
-using Lombiq.Hosting.Tenants.FeaturesGuard.Constants;
 using Lombiq.Hosting.Tenants.FeaturesGuard.Models;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using OrchardCore.Environment.Extensions;
 using OrchardCore.Environment.Extensions.Features;
 using OrchardCore.Environment.Shell;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static OrchardCore.DisplayManagement.Shapes.ShapeDebugView;
 
 namespace Lombiq.Hosting.Tenants.FeaturesGuard.Handlers;
 
@@ -18,21 +16,16 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
     private readonly IShellFeaturesManager _shellFeaturesManager;
     private readonly IOptions<ConditionallyEnabledFeaturesOptions> _conditionallyEnabledFeaturesOptions;
     private readonly ShellSettings _shellSettings;
-    private readonly IConfiguration _configuration;
-    private readonly IExtensionManager _extensionManager;
 
     public FeaturesEventHandler(
         IShellFeaturesManager shellFeaturesManager,
         IOptions<ConditionallyEnabledFeaturesOptions> conditionallyEnabledFeaturesOptions,
         ShellSettings shellSettings,
-        IConfiguration configuration,
-        IExtensionManager extensionManager)
+        IConfiguration configuration)
     {
         _shellFeaturesManager = shellFeaturesManager;
         _conditionallyEnabledFeaturesOptions = conditionallyEnabledFeaturesOptions;
         _shellSettings = shellSettings;
-        _configuration = configuration;
-        _extensionManager = extensionManager;
     }
 
     Task IFeatureEventHandler.InstallingAsync(IFeatureInfo feature) => Task.CompletedTask;
@@ -55,18 +48,19 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
 
     Task IFeatureEventHandler.UninstalledAsync(IFeatureInfo feature) => Task.CompletedTask;
 
-    public async Task EnableConditionallyEnabledFeaturesAsync(IFeatureInfo featureInfo) // EnabledAsync
+    /// <summary>
+    /// Enables conditional features (key) if one of their corresponding condition features (value) was enabled.
+    /// </summary>
+    /// <param name="featureInfo">The feature that was just enabled.</param>
+    public async Task EnableConditionallyEnabledFeaturesAsync(IFeatureInfo featureInfo)
     {
-        // CONDITIONAL FEATURE - feature A (key)
-        // CONDITION FEATURE - feature B (value)
-
         if (_shellSettings.IsDefaultShell() ||
             _conditionallyEnabledFeaturesOptions.Value.EnableFeatureIfOtherFeatureIsEnabled is not { } conditionallyEnabledFeatures)
         {
             return;
         }
 
-        var allConditionFeatureIds = new List<string>(); // need this be distinct?
+        var allConditionFeatureIds = new List<string>();
         foreach (var conditionFeatureId in conditionallyEnabledFeatures.Values)
         {
             var separatedConditionFeatureIds = conditionFeatureId.SplitByCommas();
@@ -78,8 +72,7 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
             return;
         }
 
-        // If current feature is one of the condition features, enable its corresponding conditional features if they
-        // are not already enabled.
+        // Enable conditional features if they are not already enabled.
         var allFeatures = await _shellFeaturesManager.GetAvailableFeaturesAsync();
 
         var conditionalFeatureIds = new List<string>();
@@ -103,18 +96,20 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
         var currentlyDisabledFeatures = allFeatures.Except(currentlyEnabledFeatures);
 
         // Handle multiple conditional features as well.
-        var currentlyDisabledConditionalFeautures = currentlyDisabledFeatures.Intersect(conditionalFeatures);
-        if (currentlyDisabledConditionalFeautures.Any())
+        var currentlyDisabledConditionalFeatures = currentlyDisabledFeatures.Intersect(conditionalFeatures);
+        if (currentlyDisabledConditionalFeatures.Any())
         {
-            await _shellFeaturesManager.EnableFeaturesAsync(currentlyDisabledConditionalFeautures, force: true);
+            await _shellFeaturesManager.EnableFeaturesAsync(currentlyDisabledConditionalFeatures, force: true);
         }
     }
 
-    public async Task KeepConditionallyEnabledFeaturesEnabledAsync(IFeatureInfo featureInfo) // DisabledAsync
+    /// <summary>
+    /// When a conditional feature (key) is disabled, keeps the conditional feature enabled if any of the corresponding
+    /// condition features (value) are enabled.
+    /// </summary>
+    /// <param name="featureInfo">The feature that was just disabled.</param>
+    public async Task KeepConditionallyEnabledFeaturesEnabledAsync(IFeatureInfo featureInfo)
     {
-        // CONDITIONAL FEATURE - feature A (key)
-        // CONDITION FEATURE - feature B (value)
-
         if (_shellSettings.IsDefaultShell() ||
             _conditionallyEnabledFeaturesOptions.Value.EnableFeatureIfOtherFeatureIsEnabled is not { } conditionallyEnabledFeatures)
         {
@@ -126,13 +121,22 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
             return;
         }
 
-        // If condition feature is enabled, re-enable conditional feature.
+        // Re-enable conditional feature if any its condition features are enabled.
         var allFeatures = await _shellFeaturesManager.GetAvailableFeaturesAsync();
-        var conditionFeatureId = conditionallyEnabledFeatures[featureInfo.Id];
-        var conditionFeature = allFeatures.Where(feature => feature.Id == conditionFeatureId);
+        var conditionFeatureIds = conditionallyEnabledFeatures[featureInfo.Id];
+
+        var conditionFeatureIdsList = new List<string>();
+        var separatedValues = conditionFeatureIds.SplitByCommas();
+        foreach (var separatedValue in separatedValues)
+        {
+            conditionFeatureIdsList.Add(separatedValue.Trim());
+        }
 
         var currentlyEnabledFeatures = await _shellFeaturesManager.GetEnabledFeaturesAsync();
-        if (currentlyEnabledFeatures.Contains(conditionFeature.SingleOrDefault()))
+        var conditionFeatures = allFeatures.Where(feature => conditionFeatureIdsList.Contains(feature.Id));
+
+        var currentlyEnabledConditionFeatures = currentlyEnabledFeatures.Intersect(conditionFeatures);
+        if (currentlyEnabledConditionFeatures.Any())
         {
             var conditionalFeature = allFeatures.Where(feature => feature.Id == featureInfo.Id);
 
@@ -141,18 +145,20 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
         }
     }
 
-    public async Task DisableConditionallyEnabledFeaturesAsync(IFeatureInfo featureInfo) // DisabledAsync
+    /// <summary>
+    /// When a condition feature (value) is disabled, disables the corresponding conditional features (key) if all of
+    /// their condition features are disabled.
+    /// </summary>
+    /// <param name="featureInfo">The feature that was just disabled.</param>
+    public async Task DisableConditionallyEnabledFeaturesAsync(IFeatureInfo featureInfo)
     {
-        // CONDITIONAL FEATURE - feature A (key)
-        // CONDITION FEATURE - feature B (value)
-
         if (_shellSettings.IsDefaultShell() ||
             _conditionallyEnabledFeaturesOptions.Value.EnableFeatureIfOtherFeatureIsEnabled is not { } conditionallyEnabledFeatures)
         {
             return;
         }
 
-        var allConditionFeatureIds = new List<string>(); // need this be distinct?
+        var allConditionFeatureIds = new List<string>();
         foreach (var conditionFeatureId in conditionallyEnabledFeatures.Values)
         {
             var separatedConditionFeatureIds = conditionFeatureId.SplitByCommas();
@@ -194,10 +200,10 @@ public sealed class FeaturesEventHandler : IFeatureEventHandler
         var currentlyEnabledConditionFeatures = currentlyEnabledFeatures.Intersect(conditionFeatures);
 
         // Handle multiple conditional features as well.
-        var currentlyEnabledConditionalFeautures = currentlyEnabledFeatures.Intersect(conditionalFeatures);
-        if (currentlyEnabledConditionalFeautures.Any() && !currentlyEnabledConditionFeatures.Any())
+        var currentlyEnabledConditionalFeatures = currentlyEnabledFeatures.Intersect(conditionalFeatures);
+        if (currentlyEnabledConditionalFeatures.Any() && !currentlyEnabledConditionFeatures.Any())
         {
-            await _shellFeaturesManager.DisableFeaturesAsync(currentlyEnabledConditionalFeautures);
+            await _shellFeaturesManager.DisableFeaturesAsync(currentlyEnabledConditionalFeatures);
         }
     }
 }
--- Lombiq.Hosting.Tenants.FeaturesGuard/Startup.cs
+++ Lombiq.Hosting.Tenants.FeaturesGuard/Startup.cs
@@ -1,11 +1,7 @@
 using Lombiq.Hosting.Tenants.FeaturesGuard.Constants;
 using Lombiq.Hosting.Tenants.FeaturesGuard.Handlers;
-using Lombiq.Hosting.Tenants.FeaturesGuard.Models;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Environment.Shell;
-using OrchardCore.Environment.Shell.Configuration;
-using OrchardCore.Environment.Shell.Descriptor.Models;
 using OrchardCore.Modules;
 
 namespace Lombiq.Hosting.Tenants.FeaturesGuard;
@@ -13,42 +9,6 @@ namespace Lombiq.Hosting.Tenants.FeaturesGuard;
 [Feature(FeatureNames.FeaturesGuard)]
 public class Startup : StartupBase
 {
-    private readonly IShellConfiguration _shellConfiguration;
-    private readonly ShellSettings _shellSettings;
-
-    public Startup(
-        IShellConfiguration shellConfiguration,
-        ShellSettings shellSettings)
-    {
-        _shellConfiguration = shellConfiguration;
-        _shellSettings = shellSettings;
-    }
-
-    public override void ConfigureServices(IServiceCollection services)
-    {
-        services.Configure<ConditionallyEnabledFeaturesOptions>(options =>
-            _shellConfiguration
-                .GetSection("Lombiq_Hosting_Tenants_FeaturesGuard:ConditionallyEnabledFeaturesOptions:ConditionallyEnabledFeatures")
-                .Bind(options));
-
+    public override void ConfigureServices(IServiceCollection services) =>
         services.AddScoped<IFeatureEventHandler, FeaturesEventHandler>();
-
-        // probably not usable at all
-        if (_shellSettings.IsDefaultShell()) // reversed
-        {
-            var whatsThisThen = _shellConfiguration.GetSection(
-                "Lombiq_Hosting_Tenants_FeaturesGuard:ConditionallyEnabledFeaturesOptions:ConditionallyEnabledFeatures");
-            var value = whatsThisThen.Get<ConditionallyEnabledFeaturesOptions>();
-
-            var iable = "";
-            //foreach (var alwaysEnabledFeature in alwaysEnabledFeatures.AlwaysEnabledFeatures)
-            //{
-            //    services.AddTransient(service => new ShellFeature(alwaysEnabledFeature, alwaysEnabled: true));
-            //}
-        }
-
-        // these necessary even?
-        services.AddTransient(service => new ShellFeature(FeatureNames.Users, alwaysEnabled: true));
-        services.AddTransient(service => new ShellFeature(FeatureNames.Roles, alwaysEnabled: true));
-    }
 }
