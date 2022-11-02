3cfaea89febc251c85e3520080a4340403d543e5
Gábor Pór <gabor.por@lombiq.com>
2022-11-02 15:41:21 +0100

    Moving config

--- Lombiq.Hosting.Tenants.FeaturesGuard/Startup.cs
+++ Lombiq.Hosting.Tenants.FeaturesGuard/Startup.cs
@@ -1,7 +1,10 @@
 using Lombiq.Hosting.Tenants.FeaturesGuard.Constants;
 using Lombiq.Hosting.Tenants.FeaturesGuard.Handlers;
+using Lombiq.Hosting.Tenants.FeaturesGuard.Models;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Environment.Shell;
+using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
 
 namespace Lombiq.Hosting.Tenants.FeaturesGuard;
@@ -9,6 +12,28 @@ namespace Lombiq.Hosting.Tenants.FeaturesGuard;
 [Feature(FeatureNames.FeaturesGuard)]
 public class Startup : StartupBase
 {
-    public override void ConfigureServices(IServiceCollection services) =>
+    private readonly IShellConfiguration _shellConfiguration;
+
+    public Startup(IShellConfiguration shellConfiguration) => _shellConfiguration = shellConfiguration;
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
         services.AddScoped<IFeatureEventHandler, FeaturesEventHandler>();
+
+        if (_shellConfiguration.IsAzureHosting())
+        {
+            services.Configure<ConditionallyEnabledFeaturesOptions>(options =>
+                _shellConfiguration
+                    .GetSection("Lombiq_Hosting_Tenants_FeaturesGuard:ConditionallyEnabledFeaturesOptions:ConditionallyEnabledFeatures")
+                    .Bind(options));
+        }
+
+        if (_shellConfiguration.IsUITesting())
+        {
+            services.Configure<ConditionallyEnabledFeaturesOptions>(options =>
+                _shellConfiguration
+                    .GetSection("Lombiq_Hosting_Tenants_FeaturesGuard:TestConditionallyEnabledFeaturesOptions:ConditionallyEnabledFeatures")
+                    .Bind(options));
+        }
+    }
 }
