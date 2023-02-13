686b284b95695cdd6997345e07e140aa42f619f0
Roland Márkus <roland.markus@lombiq.com>
2023-02-13 11:52:26 +0100

    Setting up test state for BG task.

--- Lombiq.Hosting.Tenants.IdleTenantManagement/Services/IdleShutdownTask.cs
+++ Lombiq.Hosting.Tenants.IdleTenantManagement/Services/IdleShutdownTask.cs
@@ -1,10 +1,5 @@
-using Lombiq.Hosting.Tenants.IdleTenantManagement.Models;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using OrchardCore.BackgroundTasks;
 using OrchardCore.Environment.Shell;
-using OrchardCore.Modules;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,53 +9,17 @@ namespace Lombiq.Hosting.Tenants.IdleTenantManagement.Services;
 [BackgroundTask(Schedule = "* * * * *", Description = "Shut down idle tenants.")]
 public class IdleShutdownTask : IBackgroundTask
 {
-    public async Task DoWorkAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken)
-    {
-        var clock = serviceProvider.GetService<IClock>();
-        var lastActiveTime = serviceProvider.GetService<ILastActiveTimeAccessor>().LastActiveDateTimeUtc;
+    private readonly IShellHost _shellHost;
+    private readonly ShellSettings _shellSettings;
 
-        var maxIdleMinutes = serviceProvider.GetService<IOptions<IdleShutdownOptions>>().Value.MaxIdleMinutes;
-
-        if (maxIdleMinutes <= 0) return;
-
-        if (lastActiveTime.AddMinutes(maxIdleMinutes) <= clock?.UtcNow)
-        {
-            var shellSettings = serviceProvider.GetService<ShellSettings>();
-            var logger = serviceProvider.GetService<ILogger<IdleShutdownTask>>();
-
-            logger?.LogWarning("Shutting down tenant \"{ShellName}\" because of idle timeout.", shellSettings?.Name);
-
-            var shellHost = serviceProvider.GetService<IShellHost>();
-
-            try
-            {
-                await shellHost.ReleaseShellContextAsync(shellSettings);
-            }
-            catch (Exception e)
-            {
-                logger?.LogError(
-                    e,
-                    "Shutting down \"{ShellName}\" because of idle timeout failed with the following exception. Another shutdown will be attempted.",
-                    shellSettings?.Name);
-
-                // If the ReleaseShellContextAsync() fails (which can happen with a DB error: then the transaction
-                // commits triggered by the dispose will fail) then while the tenant is unavailable the shell is still
-                // active in a failed state. So first we need to correctly start the tenant, then shut it down for good.
-
-                var shellSettingsManager = serviceProvider.GetService<IShellSettingsManager>();
-
-                await InvokeRestartAsync(shellSettingsManager, shellHost, shellSettings);
-                await shellHost.ReleaseShellContextAsync(shellSettings);
-            }
-        }
-    }
-
-    private static async Task InvokeRestartAsync(
-        IShellSettingsManager shellSettingsManager,
+    public IdleShutdownTask(
         IShellHost shellHost,
         ShellSettings shellSettings)
     {
-        await shellHost.UpdateShellSettingsAsync(shellSettings);
-        await shellSettingsManager.SaveSettingsAsync(shellSettings);
+        _shellHost = shellHost;
+        _shellSettings = shellSettings;
     }
+
+    public Task DoWorkAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken) =>
+        _shellHost.ReleaseShellContextAsync(_shellSettings);
 }
