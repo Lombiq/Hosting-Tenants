e035fd1347ed5c1af0a30fe07d2419edbd4aefdf
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-04-15 18:58:39 +0200

    Code refactoring

--- Lombiq.Hosting.Tenants.MediaStorageManagement/Service/DynamicMediaSizeApplicationModelConvention.cs
+++ Lombiq.Hosting.Tenants.MediaStorageManagement/Service/DynamicMediaSizeApplicationModelConvention.cs
@@ -1,33 +1,25 @@
 ﻿using Lombiq.Hosting.Tenants.MediaStorageManagement.Filters;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using OrchardCore.Media.Controllers;
+using OrchardCore.Mvc.Core.Utilities;
 using System.Linq;
 
 namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Service;
 
 public class DynamicMediaSizeApplicationModelConvention : IApplicationModelConvention
 {
-    private readonly DynamicMediaSizeActionFilter _mediaSizeFilter;
-
-    public DynamicMediaSizeApplicationModelConvention() =>
-        _mediaSizeFilter = new DynamicMediaSizeActionFilter();
-
     public void Apply(ApplicationModel application)
     {
         // Find the desired controller by name
         var targetController = application.Controllers.FirstOrDefault(controller =>
-            controller.ControllerName == "Admin" &&
-            controller.Actions.Any(action => action.ActionName == "Upload"));
+            controller.ControllerName == typeof(AdminController).ControllerName() &&
+            controller.Actions.Any(action => action.ActionName == nameof(AdminController.Upload)));
 
-        if (targetController != null)
-        {
-            // Find the desired action by name
-            var targetAction = targetController.Actions.FirstOrDefault(action => action.ActionName == "Upload");
+        // Find the desired action by name
+        var targetAction = targetController?.Actions.FirstOrDefault(action =>
+            action.ActionName == nameof(AdminController.Upload));
 
-            // Add the DynamicMediaSizeActionFilter to the action
-            if (targetAction != null)
-            {
-                targetAction.Filters.Add(_mediaSizeFilter);
-            }
-        }
+        // Add the DynamicMediaSizeActionFilter to the action
+        targetAction?.Filters.Add(new DynamicMediaSizeActionFilter());
     }
 }
