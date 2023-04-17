5e6e35ac7965e3f74f387816b462b27c16b3782f
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-04-17 23:04:30 +0200

    Adding logging exception

--- Lombiq.Hosting.Tenants.MediaStorageManagement.Tests.UI/Extensions/MediaStorageManagementExtensions.cs
+++ Lombiq.Hosting.Tenants.MediaStorageManagement.Tests.UI/Extensions/MediaStorageManagementExtensions.cs
@@ -1,4 +1,6 @@
 using Lombiq.Tests.UI.Services;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Tests.UI.Extensions;
@@ -6,14 +8,30 @@ namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Tests.UI.Extensions;
 public static class MediaStorageManagementExtensions
 {
     public static void SetMediaStorageManagementOptionsForUITest(
-        this OrchardCoreUITestExecutorConfiguration configuration) => configuration.OrchardCoreConfiguration.BeforeAppStart +=
+        this OrchardCoreUITestExecutorConfiguration configuration,
+        long maximumSpace)
+    {
+        configuration.OrchardCoreConfiguration.BeforeAppStart +=
             (_, argumentsBuilder) =>
             {
                 argumentsBuilder
-                    .AddWithValue<long>(
+                    .AddWithValue(
                         "OrchardCore:Lombiq_Hosting_Tenants_MediaStorageManagement:Media_Storage_Management_Options:MaximumSpace",
-                        57_000);
+                        maximumSpace);
 
                 return Task.CompletedTask;
             };
+
+        configuration.AssertBrowserLog =
+            logEntries =>
+            {
+                // By default, apart from some commonly known exceptions, the browser log should be empty. However,
+                // Media Storage Quota feature causes a 400 on upload if the file is over the limit, so we need to make
+                // sure not to fail on that.
+                var messagesWithoutToggle = logEntries.Where(logEntry =>
+                    !logEntry.Message.ContainsOrdinalIgnoreCase(
+                        @"/Admin/Media/Upload - Failed to load resource: the server responded with a status of 400"));
+                OrchardCoreUITestExecutorConfiguration.AssertBrowserLogIsEmpty(messagesWithoutToggle);
+            };
+    }
 }
