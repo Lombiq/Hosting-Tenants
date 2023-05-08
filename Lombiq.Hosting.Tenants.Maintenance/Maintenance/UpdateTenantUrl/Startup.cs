114e6a3165f45144e602b6d0d3be602058a8124a
Márk Bartha <barthamark@gmail.com>
2023-05-08 20:04:27 +0200

    Temporarily disabling maintenance providers

--- Lombiq.Hosting.Tenants.Maintenance/Maintenance/UpdateTenantUrl/Startup.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Maintenance/UpdateTenantUrl/Startup.cs
@@ -1,4 +1,4 @@
-using Lombiq.Hosting.Tenants.Maintenance.Services;
+using Lombiq.Hosting.Tenants.Maintenance.Constants;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Environment.Shell.Configuration;
@@ -6,7 +6,7 @@ using OrchardCore.Modules;
 
 namespace Lombiq.Hosting.Tenants.Maintenance.Maintenance.UpdateTenantUrl;
 
-[Feature(Constants.FeatureNames.UpdateTenantUrl)]
+[Feature(FeatureNames.UpdateTenantUrl)]
 public class Startup : StartupBase
 {
     private readonly IShellConfiguration _shellConfiguration;
@@ -16,8 +16,9 @@ public class Startup : StartupBase
 
     public override void ConfigureServices(IServiceCollection services)
     {
-        services.AddScoped<IMaintenanceProvider, UpdateShellRequestUrlMaintenanceProvider>();
-        services.AddScoped<IMaintenanceProvider, UpdateSiteUrlMaintenanceProvider>();
+        // Temporarily commenting out.
+        //// services.AddScoped<IMaintenanceProvider, UpdateShellRequestUrlMaintenanceProvider>();
+        //// services.AddScoped<IMaintenanceProvider, UpdateSiteUrlMaintenanceProvider>();
 
         var options = new UpdateTenantUrlMaintenanceOptions();
         var configSection = _shellConfiguration.GetSection("Lombiq_Hosting_Tenants_Maintenance:UpdateTenantUrl");
