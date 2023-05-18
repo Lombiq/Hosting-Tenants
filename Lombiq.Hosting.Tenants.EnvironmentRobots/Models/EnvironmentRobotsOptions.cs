f0c589180fd4b36ecbf442d6b1a8dd47b2347781
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-05-18 18:48:12 +0200

    Tweaking documentation

--- Lombiq.Hosting.Tenants.EnvironmentRobots/Models/EnvironmentRobotsOptions.cs
+++ Lombiq.Hosting.Tenants.EnvironmentRobots/Models/EnvironmentRobotsOptions.cs
@@ -8,9 +8,10 @@ namespace Lombiq.Hosting.Tenants.EnvironmentRobots.Models;
 public class EnvironmentRobotsOptions
 {
     /// <summary>
-    /// Gets or sets a value indicating whether to add a noindex, nofollow meta tag when the app is not running in a
-    /// production environment. It adds the tags when <see cref="IsProduction"/> is <see langword="false"/>. When set
-    /// it overrides <see cref="HostEnvironmentEnvExtensions.IsProduction"/>'s result.
+    /// Gets or sets a value indicating whether to add a meta tag and response header with noindex, nofollow values when
+    /// the app is not running in a production environment. It adds the tags when <see cref="IsProduction"/> is <see
+    /// langword="false"/>. When set it overrides <see cref="HostEnvironmentEnvExtensions.IsProduction"/>'s result,
+    /// which the module is using by default.
     /// </summary>
     public bool? IsProduction { get; set; }
 }
