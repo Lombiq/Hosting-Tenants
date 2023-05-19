ee378b1225d09baaf4915cd01513e2b2b676bdc3
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-05-19 19:45:57 +0200

    Adding TestCaseUITestContextExtensions and ConfigurationExtensions

---
+++ Lombiq.Hosting.Tenants.EnvironmentRobots.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -0,0 +1,41 @@
+using Atata;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using Shouldly;
+using System.Threading.Tasks;
+
+namespace Lombiq.Hosting.Tenants.EnvironmentRobots.Tests.UI.Extensions;
+
+public static class TestCaseUITestContextExtensions
+{
+    public static async Task TestRobotMetaTagIsMissingAsync(this UITestContext context, bool shouldBeMissing)
+    {
+        var metaTagXPath = By.XPath($"//meta[@name='robots' and @content='noindex, nofollow']").OfAnyVisibility();
+
+        await context.SignInDirectlyAsync();
+        await context.GoToHomePageAsync();
+
+        // Checking the response header with JavaScript.
+        var isXRobotsInHeader = context.Driver.ExecuteAsyncScript(@"
+            var callback = arguments[arguments.length - 1];
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', window.location.href);
+            xhr.send();
+            xhr.onload = function() {
+                var xRobotsTag = xhr.getResponseHeader('X-Robots-Tag') ?? '';
+                callback(xRobotsTag.includes ('noindex, nofollow'));
+            };");
+
+        if (missing)
+        {
+            context.Missing(metaTagXPath);
+            isXRobotsInHeader.ShouldBe(expected: false);
+        }
+        else
+        {
+            context.Exists(metaTagXPath);
+            isXRobotsInHeader.ShouldBe(expected: true);
+        }
+    }
+}
