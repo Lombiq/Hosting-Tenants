aa5731cefe043d3733659249c97921c5ce60cfdf
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-05-19 20:12:33 +0200

    Typo

--- Lombiq.Hosting.Tenants.EnvironmentRobots/Manifest.cs
+++ Lombiq.Hosting.Tenants.EnvironmentRobots/Manifest.cs
@@ -6,7 +6,7 @@ using OrchardCore.Modules.Manifest;
     Author = "Lombiq Technologies",
     Website = "https://github.com/Lombiq/Hosting-Tenants",
     Version = "0.0.1",
-    Description = "Prevents search bots from indexing non-production environments by adding a noindex, nofollow meta tag.",
+    Description = "Prevents search bots from indexing non-production environments by adding a meta tag and a response header with noindex, nofollow values.",
     Category = "Hosting"
 )]
 
