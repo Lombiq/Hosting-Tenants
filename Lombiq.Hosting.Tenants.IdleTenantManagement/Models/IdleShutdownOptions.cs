c95468babd762baa3d4d2c7ab6b6406df015d54d
Roland Márkus <roland.markus@lombiq.com>
2023-06-30 19:16:42 +0200

    Reverting default time.

--- Lombiq.Hosting.Tenants.IdleTenantManagement/Models/IdleShutdownOptions.cs
+++ Lombiq.Hosting.Tenants.IdleTenantManagement/Models/IdleShutdownOptions.cs
@@ -5,5 +5,5 @@ public class IdleShutdownOptions
     /// <summary>
     /// Gets or sets the maximum amount of idle time before the tenant shuts down.
     /// </summary>
-    public int MaxIdleMinutes { get; set; } = 1;
+    public int MaxIdleMinutes { get; set; } = 30;
 }
