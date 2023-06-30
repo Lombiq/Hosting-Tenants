7a6a4137e90eff6c520853c23ed3981182d1ffe9
Roland Márkus <roland.markus@lombiq.com>
2023-06-30 20:55:57 +0200

    Adding static string class.

---
+++ Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI/Constants/TenantNames.cs
@@ -0,0 +1,7 @@
+﻿namespace Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI.Constants;
+
+public static class TenantNames
+{
+    public static readonly string IdleTenantShutdown = nameof(IdleTenantShutdown);
+    public static readonly string idletenantprefix = nameof(idletenantprefix);
+}
