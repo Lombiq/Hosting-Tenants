c424981f76c436fb167e598ad2d890bd66afedf1
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-09-13 18:30:17 +0200

    Only limit if the preset smtp settings is being used.

--- Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/EmailQuotaService.cs
+++ Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/EmailQuotaService.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
 using OrchardCore.Email;
+using OrchardCore.Environment.Shell;
+using OrchardCore.Environment.Shell.Configuration;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.EmailQuotaManagement.Services;
@@ -12,21 +14,33 @@ public class EmailQuotaService : ISmtpService
     private readonly IStringLocalizer<EmailQuotaService> T;
     private readonly ISmtpService _smtpService;
     private readonly IQuotaService _quotaService;
+    private readonly IShellConfiguration _shellConfiguration;
+    private readonly SmtpSettings _smtpOptions;
 
     public EmailQuotaService(
         ISmtpService smtpService,
         IOptions<EmailQuotaOptions> emailQuotaOptions,
+        IOptions<SmtpSettings> smtpOptions,
         IStringLocalizer<EmailQuotaService> stringLocalizer,
-        IQuotaService quotaService)
+        IQuotaService quotaService,
+        IShellConfiguration shellConfiguration)
     {
-        _emailQuotaOptions = emailQuotaOptions.Value;
-        T = stringLocalizer;
         _smtpService = smtpService;
+        _emailQuotaOptions = emailQuotaOptions.Value;
+        _smtpOptions = smtpOptions.Value;
+        T = stringLocalizer;
         _quotaService = quotaService;
+        _shellConfiguration = shellConfiguration;
     }
 
     public async Task<SmtpResult> SendAsync(MailMessage message)
     {
+        var shellConfiguration = (SmtpSettings)_shellConfiguration.GetSection("SmtpSettings");
+        if (shellConfiguration.DefaultSender != _smtpOptions.DefaultSender)
+        {
+            return await _smtpService.SendAsync(message);
+        }
+
         var currentQuota = await _quotaService.GetCurrentQuotaAsync();
         if (_emailQuotaOptions.EmailQuota < currentQuota.CurrentEmailQuotaCount)
         {
