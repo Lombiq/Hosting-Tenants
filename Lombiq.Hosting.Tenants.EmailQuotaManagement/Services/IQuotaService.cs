0a4e0c4203f3309d8cde3974a3711071aa4f6c53
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-09-13 18:29:37 +0200

    Adding docs

--- Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/IQuotaService.cs
+++ Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/IQuotaService.cs
@@ -3,9 +3,28 @@ using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.EmailQuotaManagement.Services;
 
+/// <summary>
+/// A service that is responsible for managing the quota of sent emails.
+/// </summary>
 public interface IQuotaService
 {
+    /// <summary>
+    /// Checks if the quota is over the limit.
+    /// </summary>
+    Task<bool> IsQuotaOverTheLimitAsync();
+
+    /// <summary>
+    /// Gets the current quota.
+    /// </summary>
     Task<EmailQuota> GetCurrentQuotaAsync();
+
+    /// <summary>
+    /// Increases the given quota value.
+    /// </summary>
     void IncreaseQuota(EmailQuota emailQuota);
+
+    /// <summary>
+    /// Resets the given quota to 0.
+    /// </summary>
     void ResetQuota(EmailQuota emailQuota);
 }
