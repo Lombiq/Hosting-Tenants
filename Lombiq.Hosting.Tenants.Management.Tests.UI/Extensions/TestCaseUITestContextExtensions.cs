dbd4bcb4b150caae69848f4aea642bc1e12df9d7
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-05 17:20:24 +0200

    Adding UI test

---
+++ Lombiq.Hosting.Tenants.Management.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -0,0 +1,21 @@
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using Shouldly;
+using System.Threading.Tasks;
+
+namespace Lombiq.Hosting.Tenants.Management.Tests.UI.Extensions;
+
+public static class TestCaseUITestContextExtensions
+{
+    public static async Task TestShellSettingsEditorFeatureAsync(this UITestContext context)
+    {
+        await context.SignInDirectlyAndGoToDashboardAsync();
+        await context.GoToAdminRelativeUrlAsync("/Tenants/Edit/Default");
+        context.FillInMonacoEditor("Json_editor", "{\"TestKey:TestSubKey:TestSubOptions:Test\":\"TestValue\"}");
+        await context.ClickReliablyOnAsync(By.XPath("//button[contains(.,'Save settings')]"));
+        var editorValue = JObject.Parse(context.GetMonacoEditorText("Json_editor"));
+        editorValue.Value<string>("TestKey:TestSubKey:TestSubOptions:Test").ShouldBeAsString("TestValue");
+    }
+}
