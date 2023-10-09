62aba3c5a94c84cd28b9632bb138ee903bfd1a3a
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-09 13:10:04 +0200

    Suppressing warning

--- Lombiq.Hosting.Tenants.Management.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.Tenants.Management.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -13,9 +13,13 @@ public static class TestCaseUITestContextExtensions
     {
         await context.SignInDirectlyAsync();
         await context.GoToAdminRelativeUrlAsync("/Tenants/Edit/Default");
+
+        // Expected JSON string
+#pragma warning disable JSON002 // Probable JSON string detected
         await context.FillInEditorThenCheckValueAsync(
             "{\"TestKey:TestSubKey:TestSubOptions:Test\":\"TestValue\"}",
             "TestValue");
+#pragma warning restore JSON002 // Probable JSON string detected
         await context.FillInEditorThenCheckValueAsync(
             string.Empty,
             expectedValue: null);
