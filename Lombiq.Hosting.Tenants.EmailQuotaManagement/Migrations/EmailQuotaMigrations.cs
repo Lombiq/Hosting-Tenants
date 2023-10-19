68237211505a4a6f2e804af7ce8c81d6c9def1c6
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-19 17:39:21 +0200

    Deleting index

--- Lombiq.Hosting.Tenants.EmailQuotaManagement/Migrations/EmailQuotaMigrations.cs
+++ Lombiq.Hosting.Tenants.EmailQuotaManagement/Migrations/EmailQuotaMigrations.cs
@@ -1,6 +1,4 @@
-using Lombiq.Hosting.Tenants.EmailQuotaManagement.Indexes;
 using OrchardCore.Data.Migration;
-using System.Data.Common;
 
 namespace Lombiq.Hosting.Tenants.EmailQuotaManagement.Migrations;
 
@@ -8,8 +6,8 @@ public class EmailQuotaMigrations : DataMigration
 {
     public int UpdateFrom1()
     {
-        SchemaBuilder.AlterTable(nameof(EmailQuotaIndex), table => table
-            .AddColumn<int>(nameof(EmailQuotaIndex.LastReminderPercentage))
+        SchemaBuilder.AlterTable("EmailQuotaIndex", table => table
+            .AddColumn<int>("LastReminderPercentage")
         );
 
         return 2;
@@ -17,27 +15,9 @@ public class EmailQuotaMigrations : DataMigration
 
     public int UpdateFrom2()
     {
-        // Renaming for safety reasons.
-        TryRenameColumn("LastReminder", nameof(EmailQuotaIndex.LastReminderUtc));
-        TryRenameColumn("CurrentEmailQuotaCount", nameof(EmailQuotaIndex.CurrentEmailUsageCount));
-
         // Deleting index because it is not needed.
-        SchemaBuilder.DropTable(nameof(EmailQuotaIndex));
+        SchemaBuilder.DropTable("EmailQuotaIndex");
 
         return 3;
-    }
-
-    private void TryRenameColumn(string columnName, string newName)
-    {
-        try
-        {
-            SchemaBuilder.AlterTable(nameof(EmailQuotaIndex), table => table
-                .RenameColumn(columnName, newName)
-            );
-        }
-        catch (DbException)
-        {
-            // This is intentionally blank. If the column doesn't exist it means it already has the new name.
-        }
     }
 }
