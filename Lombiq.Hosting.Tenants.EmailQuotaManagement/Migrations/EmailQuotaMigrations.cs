de8f5b5d32e9ca806b14ced4e307435cba6a8603
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-19 18:08:44 +0200

    Suppressing is the answer

--- Lombiq.Hosting.Tenants.EmailQuotaManagement/Migrations/EmailQuotaMigrations.cs
+++ Lombiq.Hosting.Tenants.EmailQuotaManagement/Migrations/EmailQuotaMigrations.cs
@@ -4,7 +4,10 @@ namespace Lombiq.Hosting.Tenants.EmailQuotaManagement.Migrations;
 
 public class EmailQuotaMigrations : DataMigration
 {
+    // This is actually needed like this, otherwise it won't work.
+#pragma warning disable S3400
     public int Create() => 3;
+#pragma warning restore S3400
 
     public int UpdateFrom1()
     {
