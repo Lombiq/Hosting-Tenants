5a47e816b570c9422ca410733bd6608872d416a7
Roland Márkus <roland.markus@lombiq.com>
2024-01-29 14:53:48 +0100

    Keeping T.

--- Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/EmailQuotaSubjectService.cs
+++ Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/EmailQuotaSubjectService.cs
@@ -4,9 +4,11 @@ namespace Lombiq.Hosting.Tenants.EmailQuotaManagement.Services;
 
 public class EmailQuotaSubjectService(IStringLocalizer<EmailQuotaSubjectService> stringLocalizer) : IEmailQuotaSubjectService
 {
+    private readonly IStringLocalizer T = stringLocalizer;
+
     public LocalizedString GetWarningEmailSubject(int percentage) =>
-        stringLocalizer["[Warning] Your site has used {0}% of its e-mail quota", percentage];
+        T["[Warning] Your site has used {0}% of its e-mail quota", percentage];
 
     public LocalizedString GetExceededEmailSubject() =>
-        stringLocalizer["[Action Required] Your site has run over its e-mail quota"];
+        T["[Action Required] Your site has run over its e-mail quota"];
 }
