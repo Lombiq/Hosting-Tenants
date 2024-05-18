55061630c7fecf9d11fdc11850350ddc2c3f248c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-05-19 00:09:49 +0200

    Temporarily disabling file uploads

--- Lombiq.Hosting.Tenants.MediaStorageManagement/Filters/UploadFileSizeShapeFilter.cs
+++ Lombiq.Hosting.Tenants.MediaStorageManagement/Filters/UploadFileSizeShapeFilter.cs
@@ -2,28 +2,51 @@ using Lombiq.Hosting.Tenants.MediaStorageManagement.Service;
 using Lombiq.Hosting.Tenants.MediaStorageManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Localization;
+using OrchardCore.Admin;
 using OrchardCore.DisplayManagement;
 using OrchardCore.DisplayManagement.Layout;
+using OrchardCore.DisplayManagement.Notify;
 using OrchardCore.Media.Controllers;
 using OrchardCore.Mvc.Core.Utilities;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Filters;
 
-public class UploadFileSizeShapeFilter : IAsyncResultFilter
+public class UploadFileSizeShapeFilter : IAsyncResultFilter, IAsyncActionFilter
 {
     private readonly IShapeFactory _shapeFactory;
     private readonly ILayoutAccessor _layoutAccessor;
     private readonly IMediaStorageQuotaService _mediaStorageQuotaService;
+    private readonly INotifier _notifier;
+    private readonly IHtmlLocalizer H;
 
     public UploadFileSizeShapeFilter(
         IShapeFactory shapeFactory,
         ILayoutAccessor layoutAccessor,
-        IMediaStorageQuotaService mediaStorageQuotaService)
+        IMediaStorageQuotaService mediaStorageQuotaService,
+        INotifier notifier,
+        IHtmlLocalizer<UploadFileSizeShapeFilter> htmlLocalizer)
     {
         _shapeFactory = shapeFactory;
         _layoutAccessor = layoutAccessor;
         _mediaStorageQuotaService = mediaStorageQuotaService;
+        _notifier = notifier;
+        H = htmlLocalizer;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!AdminAttribute.IsApplied(context.HttpContext))
+        {
+            await next();
+            return;
+        }
+
+        await _notifier.ErrorAsync(
+            H["Due to scheduled maintenance, you can't upload files at the moment. Please try again in a few hours."]);
+
+        await next();
     }
 
     public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
--- Lombiq.Hosting.Tenants.MediaStorageManagement/Service/MediaStorageQuotaService.cs
+++ Lombiq.Hosting.Tenants.MediaStorageManagement/Service/MediaStorageQuotaService.cs
@@ -1,7 +1,5 @@
 using Lombiq.Hosting.Tenants.MediaStorageManagement.Settings;
 using Microsoft.Extensions.Options;
-using OrchardCore.Media;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Service;
@@ -9,26 +7,11 @@ namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Service;
 public class MediaStorageQuotaService : IMediaStorageQuotaService
 {
     private readonly MediaStorageManagementOptions _mediaStorageManagementOptions;
-    private readonly IMediaFileStore _mediaFileStore;
 
-    public MediaStorageQuotaService(
-        IOptions<MediaStorageManagementOptions> mediaStorageManagementOptions,
-        IMediaFileStore mediaFileStore)
-    {
+    public MediaStorageQuotaService(IOptions<MediaStorageManagementOptions> mediaStorageManagementOptions) =>
         _mediaStorageManagementOptions = mediaStorageManagementOptions.Value;
-        _mediaFileStore = mediaFileStore;
-    }
 
-    public async Task<long> GetRemainingMediaStorageQuotaBytesAsync()
-    {
-        var directoryContent = _mediaFileStore.GetDirectoryContentAsync(includeSubDirectories: true);
-
-        var listed = await directoryContent.ToListAsync();
-        var sumBytes = listed.Where(item => item.Length > 0).Sum(item => item.Length);
-        var remainingStorageQuotaBytes = GetMaxStorageQuotaBytes() - sumBytes;
-
-        return remainingStorageQuotaBytes < 0 ? 0 : remainingStorageQuotaBytes;
-    }
+    public Task<long> GetRemainingMediaStorageQuotaBytesAsync() => Task.FromResult(1L);
 
     public long GetMaxStorageQuotaBytes() => _mediaStorageManagementOptions.MaximumStorageQuotaBytes;
 }
