e1acde70abffff19b7cdc4f8fd3f62826e2c7215
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-11-28 19:35:11 +0100

    Revert "Attempting to get rid of NuGet package validation false negative"
    This reverts commit 44eeb65874df825d298abd2cf60a94f97a0666e5.

--- Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/EmailQuotaService.cs
+++ Lombiq.Hosting.Tenants.EmailQuotaManagement/Services/EmailQuotaService.cs
@@ -16,7 +16,7 @@ namespace Lombiq.Hosting.Tenants.EmailQuotaManagement.Services;
 
 // As noted under the GitHub issue https://github.com/OrchardCMS/OrchardCore/issues/15290#issuecomment-2093363619
 // SaveAsync calls could be removed due to redundancy, however, removing them actually breaks functionality.
-internal class EmailQuotaService : IEmailQuotaService
+public class EmailQuotaService : IEmailQuotaService
 {
     private readonly ISession _session;
     private readonly EmailQuotaOptions _emailQuotaOptions;
