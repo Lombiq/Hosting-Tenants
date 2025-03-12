30b9fb4d7b4742d77c42c19861e5a4d857e2f739
Benedek Farkas <benedek.farkas@lombiq.com>
2025-03-12 18:16:38 +0100

    Revert "Fixing analyzer violation without merging from dev"
    This reverts commit c1aa8a8944725fdbbf6fe415fe5ea4f5007b10dc.

--- Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI/Extensions/IdleTenantManagementExtensions.cs
+++ Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI/Extensions/IdleTenantManagementExtensions.cs
@@ -34,8 +34,7 @@ public static class IdleTenantManagementExtensions
             (_, fakeLogCollectorOptions) => fakeLogCollectorOptions.FilteredLevels.Add(LogLevel.Information);
 
         configuration.AssertAppLogsAsync = app =>
-            app.LogsShouldNotContainAsync(
-                logEntry => AppLogAssertionHelper.NotMediaCacheEntries(logEntry) && logEntry.Level != LogLevel.Information,
-                cancellationToken: configuration.TestCancellationToken);
+            app.LogsShouldNotContainAsync(logEntry =>
+                AppLogAssertionHelper.NotMediaCacheEntries(logEntry) && logEntry.Level != LogLevel.Information);
     }
 }
