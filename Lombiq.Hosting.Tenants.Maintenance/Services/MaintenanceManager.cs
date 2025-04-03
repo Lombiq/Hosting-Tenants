4115216de7bccdf10f298b1e87693803003ac245
Szabolcs Deme <szabolcs.deme@lombiq.com>
2025-04-03 23:58:11 +0200

    Unusing

--- Lombiq.Hosting.Tenants.Maintenance/Services/MaintenanceManager.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Services/MaintenanceManager.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Logging;
 using OrchardCore.Environment.Shell;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using YesSql;
