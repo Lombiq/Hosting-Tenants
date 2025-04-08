5cac1afeb40849c3f3886cef4135c5e2ae8187b8
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-04-08 15:18:48 +0200

    Add event

--- Lombiq.Hosting.Tenants.Maintenance/Controllers/StaggeredMaintenanceController.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Controllers/StaggeredMaintenanceController.cs
@@ -2,16 +2,31 @@
 using Lombiq.Hosting.Tenants.Maintenance.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OrchardCore.BackgroundJobs;
+using System;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.Maintenance.Controllers;
 
 public class StaggeredMaintenanceController : Controller
 {
+    private readonly ILogger<StaggeredMaintenanceController> _logger;
+
+    public delegate Task CompletionEventHandler(StaggeredMaintenancePart part);
+
+    public static CompletionEventHandler OnComplete { get; set; }
+
+    public StaggeredMaintenanceController(ILogger<StaggeredMaintenanceController> logger) => _logger = logger;
+
     public async Task<IActionResult> Index()
     {
         await ExecuteScheduledMaintenanceAsync();
+        OnComplete += async part =>
+        {
+            _logger.LogError("Maintenance complete! Processed: {Count}", part.ProcessedTenantsCount.Value);
+            await Task.CompletedTask;
+        };
 
         return Ok("Maintenance tasks have been scheduled.");
     }
@@ -37,9 +52,10 @@ public class StaggeredMaintenanceController : Controller
     }
 
     private Task ExecuteScheduledMaintenanceAsync(bool newVersion = false, bool reset = false) =>
-        HttpBackgroundJob.ExecuteAfterEndOfRequestAsync(nameof(StaggeredMaintenanceController), scope =>
+        HttpBackgroundJob.ExecuteAfterEndOfRequestAsync(nameof(StaggeredMaintenanceController), async scope =>
         {
             var staggeredMaintenanceService = scope.ServiceProvider.GetRequiredService<IStaggeredMaintenanceService>();
-            return staggeredMaintenanceService.RunScheduledMaintenanceForAllTenantAsync(newVersion, reset);
+            var staggeredMaintenancePart = await staggeredMaintenanceService.RunScheduledMaintenanceForAllTenantAsync(newVersion, reset);
+            await OnComplete.InvokeAsync<CompletionEventHandler>(eventHandler => eventHandler(staggeredMaintenancePart));
         });
 }
