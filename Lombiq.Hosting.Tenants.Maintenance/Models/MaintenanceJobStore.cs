4398abae0012deac829260993e97ac5d6b62ec46
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-04-08 10:08:04 +0200

    Adding maintenance job store, for cancellation

---
+++ Lombiq.Hosting.Tenants.Maintenance/Models/MaintenanceJobStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lombiq.Hosting.Tenants.Maintenance.Models;
+
+public static class MaintenanceJobStore
+{
+    private static readonly ConcurrentDictionary<string, bool> _cancelFlags = new();
+
+    public static void RequestCancel(string jobId) => _cancelFlags[jobId] = true;
+
+    public static bool IsCancelled(string jobId) => _cancelFlags.TryGetValue(jobId, out var cancel) && cancel;
+
+    public static void Clear(string jobId) => _cancelFlags.TryRemove(jobId, out _);
+}
