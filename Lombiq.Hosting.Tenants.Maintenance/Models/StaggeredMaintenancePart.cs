631d3a475a066c1876d2308343398c0966fc2cad
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-04-08 15:19:17 +0200

    Additional part functions

--- Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenancePart.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenancePart.cs
@@ -1,6 +1,7 @@
 ﻿using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Lombiq.Hosting.Tenants.Maintenance.Models;
@@ -14,6 +15,27 @@ public class StaggeredMaintenancePart : ContentPart
     public NumericField CurrentVersion { get; set; } = new() { Value = 0 };
     public IList<string> ProcessedTenantIds { get; } = [];
 
+    public HtmlField ErrorLogsHtmlField { get; set; } = new()
+    {
+        Html = JsonSerializer.Serialize(new Dictionary<string, string>(), JOptions.CamelCaseIndented),
+    };
+
     [JsonIgnore]
     public IDictionary<string, string> ErrorLogs { get; } = new Dictionary<string, string>();
+
+    public void AddErrorLog(string tenantId, string error)
+    {
+        ErrorLogs.Add(tenantId, error);
+        ErrorLogsHtmlField.Html = JsonSerializer.Serialize(ErrorLogs, JOptions.CamelCaseIndented);
+    }
+
+    public void Clear()
+    {
+        ProcessedTenantIds.Clear();
+        AllTenantCount.Value = 0;
+        ProcessedTenantsCount.Value = 0;
+        ProgressPercentage.Value = 0;
+        ErrorLogs.Clear();
+        ErrorLogsHtmlField.Html = JsonSerializer.Serialize(ErrorLogs, JOptions.CamelCaseIndented);
+    }
 }
--- Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenanceTenantStatusPart.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenanceTenantStatusPart.cs
@@ -1,9 +1,30 @@
-﻿using OrchardCore.ContentManagement;
+﻿using OrchardCore.ContentFields.Fields;
+using OrchardCore.ContentManagement;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Lombiq.Hosting.Tenants.Maintenance.Models;
 
 public class StaggeredMaintenanceTenantStatusPart : ContentPart
 {
-    public IDictionary<string, string> Versions { get; } = new Dictionary<string, string>();
+    public HtmlField VersionsHtmlField { get; set; } = new()
+    {
+        Html = JsonSerializer.Serialize(new Dictionary<string, string>()),
+    };
+
+    [JsonIgnore]
+    public IDictionary<string, string> Versions { get; private set; } = new Dictionary<string, string>();
+
+    public void AddVersion(string tenantId, string version)
+    {
+        if (!Versions.Any())
+        {
+            Versions = JsonSerializer.Deserialize<IDictionary<string, string>>(VersionsHtmlField.Html);
+        }
+
+        Versions[tenantId] = version;
+        VersionsHtmlField.Html = JsonSerializer.Serialize(Versions, JOptions.CamelCaseIndented);
+    }
 }
