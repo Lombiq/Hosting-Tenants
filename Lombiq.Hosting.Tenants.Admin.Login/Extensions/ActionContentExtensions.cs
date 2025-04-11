7e72d06aeb7d0e9a97f31dad70eb7e93f7bedba5
Benedek Farkas <benedek.farkas@lombiq.com>
2025-04-11 21:56:47 +0200

    Apply suggestions from code review
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- Lombiq.Hosting.Tenants.Admin.Login/Extensions/ActionContentExtensions.cs
+++ Lombiq.Hosting.Tenants.Admin.Login/Extensions/ActionContentExtensions.cs
@@ -4,7 +4,7 @@ using OrchardCore.Tenants.Controllers;
 
 namespace Lombiq.Hosting.Tenants.Admin.Login.Extensions;
 
-public static class ActionContentExtensions
+internal static class ActionContentExtensions
 {
     public static bool IsTenantEditRoute(this ActionContext context) =>
         context.IsMvcRoute(
