d49c3152c2575fa836496ba453e0a34752845ccd
Krisztián Németh <nkrisz5@gmail.com>
2025-04-24 10:03:40 +0200

    Update Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenanceOptions.cs
    Co-authored-by: Benedek Farkas <benedek.farkas@lombiq.com>

--- Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenanceOptions.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Models/StaggeredMaintenanceOptions.cs
@@ -2,7 +2,7 @@ namespace Lombiq.Hosting.Tenants.Maintenance.Models;
 
 public class StaggeredMaintenanceOptions
 {
-    public int? MillisecondBetweenBatches { get; set; }
+    public int? BatchIntervalSeconds { get; set; }
     public int? ProcessingStep { get; set; }
     public bool? RunParallel { get; set; }
 }
