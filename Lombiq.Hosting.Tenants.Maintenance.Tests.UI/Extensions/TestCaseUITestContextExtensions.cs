e0cc62e251414ae94ecabef4d89b7a7920b6e1ba
Sára El-Saig <sara.el-saig@lombiq.com>
2025-07-31 01:21:40 +0200

    Get rid of GoToDashboardAsync.

--- Lombiq.Hosting.Tenants.Maintenance.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.Tenants.Maintenance.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -45,7 +45,7 @@ public static class TestCaseUITestContextExtensions
         await context.ClickReliablyOnByLinkTextAsync("Multi-Tenancy");
         await context.ClickReliablyOnByLinkTextAsync("Tenants");
         await context.ClickReliablyOnByLinkTextAsync("StaggeredTenantWakeUp");
-        await context.GoToDashboardAsync();
+        await context.GoToAdminAsync();
 
         // Put the tenant into idle mode, to see if it works that way also.
         await context.Application.UsingScopeAsync(
