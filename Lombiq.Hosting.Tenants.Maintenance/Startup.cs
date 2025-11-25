88176e24536784bb056630d02858921792866ab4
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-11-25 21:36:29 +0100

    Fix build

--- Lombiq.Hosting.Tenants.Maintenance/Startup.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Startup.cs
@@ -1,22 +1,11 @@
 using Lombiq.HelpfulLibraries.Common.DependencyInjection;
-using Lombiq.HelpfulLibraries.OrchardCore.Mvc;
 using Lombiq.Hosting.Tenants.Maintenance.Constants;
-using Lombiq.Hosting.Tenants.Maintenance.Handlers;
 using Lombiq.Hosting.Tenants.Maintenance.Indexes;
-using Lombiq.Hosting.Tenants.Maintenance.Models;
 using Lombiq.Hosting.Tenants.Maintenance.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using OrchardCore.ContentManagement;
-using OrchardCore.ContentManagement.Display.ContentDisplay;
-using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.Data;
 using OrchardCore.Data.Migration;
-using OrchardCore.Environment.Shell;
-using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
-using OrchardCore.Navigation;
-using OrchardCore.ResourceManagement;
 
 namespace Lombiq.Hosting.Tenants.Maintenance;
 
@@ -33,5 +22,4 @@ public sealed class Startup : StartupBase
         services.AddScoped<IModularTenantEvents, MaintenanceRunnerService>();
         services.AddScoped<IMaintenanceManager, MaintenanceManager>();
     }
-}
 }
