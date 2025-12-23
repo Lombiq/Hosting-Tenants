d5d6d521782ca762ba1521b934e019abe1c0d764
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-12-24 00:09:48 +0100

    Purging via IMediaFileStoreCacheFileProvider

--- Lombiq.Hosting.Tenants.Maintenance/Maintenance/PurgeMediaCache/PurgeMediaCacheMaintenanceProvider.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Maintenance/PurgeMediaCache/PurgeMediaCacheMaintenanceProvider.cs
@@ -1,11 +1,7 @@
-using Lombiq.Hosting.Tenants.Maintenance.Extensions;
 using Lombiq.Hosting.Tenants.Maintenance.Models;
 using Lombiq.Hosting.Tenants.Maintenance.Services;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Options;
-using OrchardCore.Environment.Shell;
-using OrchardCore.Media.Core;
-using System.IO;
+using OrchardCore.Media;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.Tenants.Maintenance.Maintenance.PurgeMediaCache;
@@ -13,38 +9,23 @@ namespace Lombiq.Hosting.Tenants.Maintenance.Maintenance.PurgeMediaCache;
 public class PurgeMediaCacheMaintenanceProvider : MaintenanceProviderBase
 {
     private readonly IOptions<PurgeMediaCacheMaintenanceOptions> _options;
-    private readonly IWebHostEnvironment _webHostEnvironment;
-    private readonly ShellSettings _shellSettings;
+    private readonly IMediaFileStoreCacheFileProvider _mediaFileStoreCacheFileProvider;
 
     public PurgeMediaCacheMaintenanceProvider(
         IOptions<PurgeMediaCacheMaintenanceOptions> options,
-        IWebHostEnvironment webHostEnvironment,
-        ShellSettings shellSettings)
+        IMediaFileStoreCacheFileProvider mediaFileStoreCacheFileProvider)
     {
         _options = options;
-        _webHostEnvironment = webHostEnvironment;
-        _shellSettings = shellSettings;
+        _mediaFileStoreCacheFileProvider = mediaFileStoreCacheFileProvider;
     }
 
     public override Task<bool> ShouldExecuteAsync(MaintenanceTaskExecutionContext context) =>
         Task.FromResult(
             _options.Value.IsEnabled &&
-            _shellSettings.Name == ShellSettings.DefaultShellName &&
             // Should run on every swap, although not every new deployment. No better simple way to check for this, so
             // running on every deployment anyway.
             context.LatestExecution?.BuildVersion != context.CurrentExecution.BuildVersion);
 
-    public override Task ExecuteAsync(MaintenanceTaskExecutionContext context)
-    {
-        var cachePath = Path.Combine(
-            _webHostEnvironment.WebRootPath,
-            DefaultMediaFileStoreCacheFileProvider.AssetsCachePath);
-
-        if (Directory.Exists(cachePath))
-        {
-            Directory.Delete(cachePath, recursive: true);
-        }
-
-        return Task.CompletedTask;
-    }
+    public override Task ExecuteAsync(MaintenanceTaskExecutionContext context) =>
+        _mediaFileStoreCacheFileProvider.PurgeAsync();
 }
