29a34d5f9853ca36a0cd1be9770b80c173bdedd8
Sára El-Saig <sara.el-saig@lombiq.com>
2026-02-22 19:09:00 +0100

    Fix scope issue.

--- Lombiq.Hosting.Tenants.MediaStorageManagement/Handlers/MediaStorageQuotaHandler.cs
+++ Lombiq.Hosting.Tenants.MediaStorageManagement/Handlers/MediaStorageQuotaHandler.cs
@@ -1,4 +1,6 @@
 using Lombiq.Hosting.Tenants.MediaStorageManagement.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Media.Events;
 using System.Threading.Tasks;
 
@@ -6,11 +8,16 @@ namespace Lombiq.Hosting.Tenants.MediaStorageManagement.Handlers;
 
 public class MediaStorageQuotaHandler : IMediaEventHandler
 {
-    private readonly IMediaStorageQuotaService _mediaStorageQuotaService;
+    private readonly IHttpContextAccessor _hca;
 
-    public MediaStorageQuotaHandler(IMediaStorageQuotaService mediaStorageQuotaService) =>
-        _mediaStorageQuotaService = mediaStorageQuotaService;
+    public MediaStorageQuotaHandler(IHttpContextAccessor hca) =>
+        _hca = hca;
 
-    public async Task MediaPermittedStorageAsync(MediaPermittedStorageContext context) =>
-        context.Constrain(await _mediaStorageQuotaService.GetRemainingMediaStorageQuotaBytesAsync());
+    public async Task MediaPermittedStorageAsync(MediaPermittedStorageContext context)
+    {
+        if (_hca.HttpContext?.RequestServices.GetService<IMediaStorageQuotaService>() is { } mediaStorageQuotaService)
+        {
+            context.Constrain(await mediaStorageQuotaService.GetRemainingMediaStorageQuotaBytesAsync());
+        }
+    }
 }
