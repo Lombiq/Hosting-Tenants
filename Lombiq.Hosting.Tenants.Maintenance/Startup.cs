5b5ce69fd70771db229755fa9e047c356c584405
Sára El-Saig <sara.el-saig@lombiq.com>
2026-04-16 23:45:24 +0200

    Re-add the migration that fixes old "QueryElasticsearch{0}Index"
    it's no longer done by OC

--- Lombiq.Hosting.Tenants.Maintenance/Startup.cs
+++ Lombiq.Hosting.Tenants.Maintenance/Startup.cs
@@ -18,6 +18,8 @@ public sealed class Startup : StartupBase
 
         services.AddIndexProvider<MaintenanceTaskExecutionIndexProvider>();
         services.AddDataMigration<Migrations>();
+        
+        services.AddDataMigration<QueryElasticsearchMigration>();
 
         services.AddScoped<IModularTenantEvents, MaintenanceRunnerService>();
         services.AddScoped<IMaintenanceManager, MaintenanceManager>();
