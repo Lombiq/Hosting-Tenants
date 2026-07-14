404ec2ad85938da33131ea5a9b90e0b2fd5a73b4
Sára El-Saig <sara.el-saig@lombiq.com>
2026-07-14 14:19:57 +0200

    Add AddElasticsearchFeatures extension method.

---
+++ Lombiq.Hosting.Tenants.Maintenance/Maintenance/ElasticsearchIndices/OrchardCoreBuilderExtensions.cs
@@ -0,0 +1,26 @@
+using Lombiq.Hosting.Tenants.Maintenance.Constants;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lombiq.Hosting.Tenants.Maintenance.Maintenance.ElasticsearchIndices;
+
+public static class OrchardCoreBuilderExtensions
+{
+    /// <summary>
+    /// Adds Elasticsearch setup and tenant features, if <paramref name="useElasticSearch"/> is <see langword="true"/>.
+    /// </summary>
+    public static OrchardCoreBuilder AddElasticsearchFeatures(this OrchardCoreBuilder builder, IWebHostEnvironment environment, bool useElasticSearch)
+    {
+        if (!useElasticSearch) return builder;
+
+        builder.AddSetupFeatures(FeatureNames.DeleteElasticsearchIndicesBeforeSetup);
+
+        if (environment.IsStaging())
+        {
+            builder.AddTenantFeatures(FeatureNames.DeleteOrRebuildElasticsearchIndices);
+        }
+
+        return builder;
+    }
+}
