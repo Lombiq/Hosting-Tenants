48f22022bde6b49935b3f0bd70f4c7dcdf7f50e2
Sára El-Saig <sara.el-saig@lombiq.com>
2026-09-22 17:44:44 +0200

    Check tenant health hourly.

--- Lombiq.Hosting.Tenants.HealthChecks/Services/TenantCheckerBackgroundTask.cs
+++ Lombiq.Hosting.Tenants.HealthChecks/Services/TenantCheckerBackgroundTask.cs
@@ -1,16 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using OrchardCore.BackgroundTasks;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Lombiq.Hosting.Tenants.Management.Services;
+namespace Lombiq.Hosting.Tenants.HealthChecks.Services;
 
 [BackgroundTask(Schedule = "0 * * * *", Description = "Check tenant health hourly.")]
 public class TenantCheckerBackgroundTask : IBackgroundTask
 {
     public Task DoWorkAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
-        // TODO periodically run health checks using HealthCheckService on running tenants. Also trigger this when the tenant starts.
-        throw new NotImplementedException();
+        var service = serviceProvider.GetRequiredService<HealthCheckService>();
+        return service.CheckHealthAsync(cancellationToken);
     }
 }
